51587e858891b9c1beb9d2f3b47ee868c7b9af3c
vszal <>
2021-01-16 13:53:42 -0800

    major refactor for better error handling, removed dependency on zipcodes which didn't really work

--- app.py
+++ app.py
@@ -1,41 +1,68 @@
 import os
 import re
 import requests
-import zipcodes
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    #if no zipcode in URL, guess based on geolocation
+    # grab IP from env variable
     ip_address = get_ip()
-    if ip_address =='': #ip_address is undefined, likely missing environment variable
-        return '<strong>IP address is undefined. Make sure DEV_EXT_IP environment variable is set</strong>'
-    else:
+    if ip_address =='': #ip_address is undefined, likely missing fall-back environment variable
+        return render_template('error.html', no_ip='1')
+    
+    #if no zipcode in URL, guess based on geolocation
+    try: 
         zipcode, country = get_location_by_ip(ip_address)
-        if country != 'US':
-            return render_template('error.html', country=country)
-        #query the covid data API    
-        county, lat, lng, covid_data = get_covid_data(zipcode)
-        return render_template('index.html', zipcode=zipcode, country=country, county=county, lat=lat, lng=lng, covid_data=covid_data)        
+    except:
+        return render_template('error.html', error_status='There is a problem connecting to the geolocation API')
 
+    if country != 'US':
+        return render_template('error.html', country=country)
+    #query the covid data API  
+    try:
+        covid_json_data = get_covid_data_from_zip(zipcode)
+    except:
+        return render_template('error.html', error_status='There is a problem connecting to the COVID data API')
+
+    # parse the data and format, get census data
+    try:
+        county, lat, lng, covid_data = parse_covid_data(covid_json_data)
+    except:
+        return render_template('error.html', error_status='There is a problem connecting to the census data API')
+    else:
+        # Success path
+        return render_template('index.html', zipcode=zipcode, county=county, lat=lat, lng=lng, covid_data=covid_data)
+      
 @app.route('/<zipcode>')
 def zip(zipcode):
-    # Test if the inputted zipcode is real using the package "zipcodes"
-    try:
-        zipcodes.is_real(zipcode)
-    except:
+    # Make sure zipcode matches 5 digit format
+    zipcode_format_test = re.search(r'[5]\d', zipcode)
+    if zipcode_format_test is not None:
         return render_template('error.html', zipcode=zipcode)
-    #query the covid data and population APIs    
-    county, lat, lng, covid_data = get_covid_data(zipcode)
-    return render_template('index.html', zipcode=zipcode, county=county, lat=lat, lng=lng, covid_data=covid_data)
+
+    try:
+        covid_json_data = get_covid_data_from_zip(zipcode)
+    except:
+        return render_template('error.html', error_status='There is a problem connecting to the COVID data API')
+    # error if entered zipcode doesn't exist
+    if len(covid_json_data)==0: 
+        return render_template('error.html', zipcode=zipcode)
+    # parse the data and format, get census data
+    try:
+        county, lat, lng, covid_data = parse_covid_data(covid_json_data)
+    except:
+        return render_template('error.html', error_status='There is a problem connecting to the census data API')
+    else:
+        # Success path
+        return render_template('index.html', zipcode=zipcode, county=county, lat=lat, lng=lng, covid_data=covid_data)
 
 def get_ip():
     # GCP Cloud Run needs X-Forwarded_For
     ip_address = request.headers.get('X-Forwarded-For', request.remote_addr) 
     # For dev testing, get external IP from environment variable
-    if (re.search('^192|^127|^172|^10\.', ip_address)):
+    if (re.search('^192|^127|^0\.|^172|^10\.', ip_address)):
         ip_address = os.environ.get('DEV_EXT_IP')  
     return ip_address
 
@@ -43,26 +70,39 @@ def get_location_by_ip(ip_address):
     try:
         loc_api = requests.get(f'http://ip-api.com/json/{ip_address}')
         loc_api.raise_for_status()
+    except requests.exceptions.RequestException as err:
+        raise
     except requests.exceptions.HTTPError as err:
-        raise SystemExit(err)
+        raise 
     loc_data = loc_api.json()
     zipcode = loc_data['zip']
     country = loc_data['countryCode']
     return zipcode, country
 
-def get_covid_data(zipcode):
+def get_covid_data_from_zip(zipcode):
+    #Try the covid API
     try:
         covid_api = requests.get(f'https://localcoviddata.com/covid19/v1/cases/newYorkTimes?zipCode={zipcode}&daysInPast=7')
         covid_api.raise_for_status()
+    except requests.exceptions.RequestException as err:
+        raise
     except requests.exceptions.HTTPError as err:
-        raise SystemExit(err)
-    cov_data = covid_api.json()
-    county = cov_data["counties"][0]["countyName"]
-    lat = cov_data["counties"][0]["geo"]["leftBottomLatLong"]
-    lng = cov_data["counties"][0]["geo"]["rightTopLatLong"]
-    data = cov_data["counties"][0]["historicData"]
+        raise 
+    else:
+        return covid_api.json()
+
+def parse_covid_data(covid_json):
+    county = covid_json["counties"][0]["countyName"]
+    lat = covid_json["counties"][0]["geo"]["leftBottomLatLong"]
+    lng = covid_json["counties"][0]["geo"]["rightTopLatLong"]
+    data = covid_json["counties"][0]["historicData"]
     # get population for county by coordinates
-    county_population = get_census_data(lat, lng)
+    try: 
+        county_population = get_census_data(lat, lng)
+    except requests.exceptions.RequestException as err:
+        raise
+    except requests.exceptions.HTTPError as err:
+        raise 
     # calculate deltas and add to an array
     n = len(data) - 2 # since we're doing deltas and i starts at zero
     i = 0
@@ -88,8 +128,10 @@ def get_census_data(lat, lng):
     try:
         geocode = requests.get(f'https://geo.fcc.gov/api/census/block/find?latitude={lat}&longitude={lng}&showall=true&format=json')
         geocode.raise_for_status()
+    except requests.exceptions.RequestException as err:
+        raise
     except requests.exceptions.HTTPError as err:
-        raise SystemExit(err) 
+        raise 
 
     geocodej = geocode.json()
     FIPS_code = geocodej["County"]["FIPS"]
@@ -99,11 +141,14 @@ def get_census_data(lat, lng):
     # get county population from the census API
     try:
         population = requests.get(f'https://api.census.gov/data/2019/pep/population?get=POP,NAME,STATE&for=county:{county_code}&in=state:{state_code}')
+        population.raise_for_status()
+    except requests.exceptions.RequestException as err:
+        raise
     except requests.exceptions.HTTPError as err:
-        raise SystemExit(err) 
+        raise  
 
-    popj = population.json()
-    county_population = int(popj[1][0])
+    pop_json = population.json()
+    county_population = int(pop_json[1][0])
     return county_population
 
 if __name__ == '__main__':
