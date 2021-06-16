5bc4a1ce6c0c86f31d8a35d3de3390967217c34b
Dave Stanke <davidstanke@gmail.com>
2021-06-16 02:50:29 +0000

    Use terraform for provisioning

---
+++ main.tf
@@ -0,0 +1,44 @@
+resource "null_resource" "app_container" {
+    provisioner "local-exec" {
+        command = "gcloud builds submit --tag=gcr.io/${var.google_project_id}/covid-stats"
+    }
+}
+
+resource "google_cloud_run_service" "covid_stats" {
+  name     = "covid-stats"
+  location = var.google_region
+
+  template {
+    spec {
+      containers {
+        image = "gcr.io/${var.google_project_id}/covid-stats"
+        # env {
+        #   name  = "PROJECT_NAME"
+        #   value = var.google_project_id
+        # }
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+
+  autogenerate_revision_name = true
+
+  depends_on = [
+    null_resource.app_container
+  ]
+
+}
+
+resource "google_cloud_run_service_iam_binding" "noauth" {
+  location = var.google_region
+  project  = var.google_project_id
+  service  = "covid-stats"
+
+  role       = "roles/run.invoker"
+  members    = ["allUsers"]
+  depends_on = [google_cloud_run_service.covid_stats]
+}
